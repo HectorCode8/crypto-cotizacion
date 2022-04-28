30a29a179c9a3a7f2700e4d87866c7a7b5786cb1
Hector Haro H <hector8.haro@gmail.com>
2022-04-28 01:50:19 -0500

    add style to select and input components

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import useSelectMonedas from '../hooks/useSelectMonedas'
+import { monedas } from '../data/monedas'
 
 const InputSubmit = styled.input`
     background-color: #9497FF;
@@ -13,6 +14,7 @@ const InputSubmit = styled.input`
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
 
     &:hover {
         background-color: #7A7DFE;
@@ -21,21 +23,17 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
- 
-    const [ SelectMonedas ] = useSelectMonedas('Elige tu Moneda')
-    
-    
+    const [ SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas)
+    return (
+    <form>
 
-  return (
-   <form>
+        <SelectMonedas />
+        
 
-       <SelectMonedas />
-       
-
-       <InputSubmit 
-            type="submit" 
-            value="Cotizar"/>
-   </form>
+        <InputSubmit 
+                type="submit" 
+                value="Cotizar"/>
+    </form>
   )
 }
 

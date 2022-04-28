37c89e39b5ba1495dab3f23220d24ac2a4dc6c7e
Hector Haro H <hector8.haro@gmail.com>
2022-04-28 01:30:16 -0500

    Create custom hook useSelectMonedas

---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const useSelectMonedas = () => {
+    
+    const SelectMonedas = () => {
+        console.log('desde custom hook')   
+    }
+    return [SelectMonedas]
+}
+
+export default useSelectMonedas
